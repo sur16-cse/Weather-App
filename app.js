1b5ef950068e5488934757d397a0932de9ffc299
surbhi kumari <surbhikumaridav@gmail.com>
2021-09-15 16:14:06 +0530

    background unsplash image updated

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var search = document.getElementById("upper-search");
+var dSearch = document.querySelector('.main-sc-input');
 var butup = document.getElementById("upper-btn");
+var wtBtn = document.getElementsByClassName("wt-btn")[0];
 var mainBody = document.getElementsByClassName("main-container")[0];
 var dataBody = document.getElementsByClassName("weather-data")[0];
+var locationfinder= document.querySelector(".icon");
+var background=document.getElementsByTagName('body')[0];
+var forecast=document.getElementsByClassName('fc-btn')[0];
+var forecastdata=document.getElementById('forecast-data');
+var timeEle=document.getElementById('time');
+var dateEle=document.getElementById('date');
 var submit = true;
-
 var Celsius = "Celsius";
 var Fahrenheit = "Fahrenheit";
 var celtofahren;
+
 async function getdata(data) {
   const res = await fetch(
     "http://api.openweathermap.org/data/2.5/weather?q=" +
@@ -24,8 +32,6 @@ async function getdata(data) {
 }
 
 var toggle=true;
-
-
 function format(sun) {
   let date = new Date(sun * 1000);
   var timestr = date.toLocaleTimeString([], {
@@ -48,7 +54,8 @@ function getweather(get) {
     <div class="city-data" id = "feel"><img src="https://img.icons8.com/doodle/20/000000/climate-change.png"/>  Feel:
      ${
       (get.main.feels_like - 273).toFixed(2)
-      } ${toggle?Celsius:Fahrenheit}</div>
+      
+    } ${toggle?Celsius:Fahrenheit}</div>
     <div class="city-data"><img src="http://openweathermap.org/img/wn/${
       get.weather[0].icon
     }.png">  ${get.weather[0].main}</div>
@@ -63,7 +70,7 @@ function getweather(get) {
     } %</div>
     <div class="city-data"><img src="https://img.icons8.com/clouds/20/000000/wind.png"/>  Wind Speed: ${
       get.wind.speed
-    } m/sec</div>
+    } km/hr</div>
     <div class="city-data"><img src="https://img.icons8.com/external-justicon-lineal-color-justicon/20/000000/external-cloud-weather-justicon-lineal-color-justicon-1.png"/>  Cloudiness: ${
       get.clouds.all
     }%</div>
@@ -101,8 +108,115 @@ butup.addEventListener("click", (e) => {
 
   if (data) {
     getdata(data);
+    backgroundimage(data);
     search.value = " ";
     mainBody.style.display = "none";
     dataBody.style.display = "flex";
   }
 });
+wtBtn.addEventListener("click", (e) => {
+  data = dSearch.value;
+
+  if (data) {
+    getdata(data);
+    backgroundimage(data);
+    dSearch.value = " ";
+    mainBody.style.display = "none";
+    dataBody.style.display = "flex";
+  }
+});
+
+
+//
+
+(function() {
+  var placesAutocomplete = places({
+    appId: 'plBAKYUGY2FI',
+    apiKey: 'f7093e7853be493e407d46a434f9c54b',
+    container: document.querySelector('.main-sc-input'),
+    templates: {
+      value: function(suggestion) {
+        return suggestion.name;
+      }
+    }
+  }).configure({
+    type: 'city'
+  });
+})();
+
+ locationfinder.addEventListener('click',(e)=>{
+navigator.geolocation.getCurrentPosition((position)=>{
+  var lat  = position.coords.latitude;
+  var lon = position.coords.longitude;
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=f190a0abeaeaaf5380c205502c29c38c`)
+  .then(res=>res.json())
+  .then(val => {
+    console.log(val);
+    //getweather(val);
+  })
+   .catch(error=>
+     alert("Geolocation is not working"))
+})
+}) 
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+setInterval(() => {
+  const time = new Date();
+  const month = time.getMonth();
+  const date = time.getDate();
+  const day = time.getDay();
+  const hour = time.getHours();
+  const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+  const minutes = time.getMinutes();
+  const ampm = hour >=12 ? 'PM' : 'AM'
+
+  timeEle.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+  dateEle.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
+
+ async function backgroundimage(data) {
+  let randomImage = await getNewImage(data);
+      background.style.backgroundImage= `url(${randomImage})`;
+ }
+
+  async function getNewImage(data) {
+
+    let randomNumber = Math.floor(Math.random() * 10);
+    return fetch('https://api.unsplash.com/search/photos?query='+
+  data+'&client_id=wWMUmSBXtnSi_fwXg0ceNHyV0GMZbTrIpIH6eVCRNK8')
+      .then((response) => response.json())
+      .then((databack) => {
+        let allImages = databack.results[randomNumber];
+        return allImages.urls.regular;
+      });
+  }
+
+
+forecast.addEventListener('click',(e)=>{
+    data = dSearch.value;
+
+    if (data) {
+      //getforecastdata(data);
+      backgroundimage(data);
+      dSearch.value = " ";
+      mainBody.style.display = "none";
+      dataBody.style.display = "none";
+      forecastdata.style.display="flex";
+      document.getElementsByTagName('footer')[0].style.display="none";
+    }
+})
+
+// async function getforecastdata(data) {
+//   const res=await fetch('')
+// }
+
+
+
+
+
+
+
